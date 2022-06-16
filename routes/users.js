5c2f067e1b086aad873ac2d6e4c971d16e975594
Nikolay Petrovskiy <n.kzn@yandex.ru>
2022-06-16 18:29:51 +0300

    feat: add routes users

---
+++ routes/users.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const {
+  getUsers,
+  getUserId,
+  createUser,
+} = require('../controllers/users');
+
+router.get('/', getUsers);
+router.get('/:userId', getUserId);
+router.post('/', createUser);
+
+module.exports = router;
