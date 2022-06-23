0ca664b553251795db65ec1347e5c753ffa45bcd
Nikolay Petrovskiy <n.kzn@yandex.ru>
2022-06-23 06:48:38 +0300

    fix: bug fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { login, createUser } = require('./routes/users');
+const { login, createUser } = require('./controllers/users');
 const cardsRoutes = require('./routes/cards');
 const auth = require('./middlewares/auth');
 
