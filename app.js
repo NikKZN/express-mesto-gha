27836a8b1a777ac551d5edaa845ce2dee4d2710d
Nikolay Petrovskiy <n.kzn@yandex.ru>
2022-07-03 06:50:06 +0300

    feat: add logger

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { login, createUser } = require('./controllers/users');
 const cardsRoutes = require('./routes/cards');
 const usersRoutes = require('./routes/users');
@@ -18,6 +19,8 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
+app.use(requestLogger);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -31,6 +34,8 @@ app.use('/users', usersRoutes);
 app.use('*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена!'));
 });
+
+app.use(errorLogger);
 
 app.use(errors());
 
